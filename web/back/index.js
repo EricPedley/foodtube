95527d6457de77fd68646539be27a0356df85821
Eric <ericpedley@gmail.com>
2020-01-26 18:39:15 -0800

    changed port

--- web/back/index.js
+++ web/back/index.js
@@ -2,11 +2,11 @@ let express=require('express');
 const cors = require('cors');
 var app = express();
 app.use(express.json({type:"*/*"}));
-app.use(express.static('../front'));
+app.use(express.static('../front/index.html'));
 app.use(cors());
 //app.post('/query-recipe', require('query-recipe'));
 app.get('/all-recipes', require('./all-recipes'));
 app.post('/add-recipes', require('./add-recipes'));
-app.listen(3000);
+app.listen(process.env.PORT || 5000);
 
 console.log("app started");
