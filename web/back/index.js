ca07ed737085e6e7078fa519af68669f403be7b0
Eric <ericpedley@gmail.com>
2020-01-31 20:19:01 -0800

    fixed port error

--- web/back/index.js
+++ web/back/index.js
@@ -2,11 +2,12 @@ let express=require('express');
 const cors = require('cors');
 var app = express();
 app.use(express.json({type:"*/*"}));
-app.use(express.static('../front'));
+app.use(express.static('web/front'));
 app.use(cors());
 //app.post('/query-recipe', require('query-recipe'));
 app.get('/all-recipes', require('./all-recipes'));
 app.post('/add-recipes', require('./add-recipes'));
-app.listen(3000);
+let port = process.env.PORT || 5000
+app.listen(port);
 
-console.log("listening on port 3000");
+console.log(`listening on ${port}`);
